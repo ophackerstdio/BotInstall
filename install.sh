c622355e546c0c65af7acb5eae777c145cdca394
OP Haseeb <ophaseeb269@gmail.com>
2025-06-03 00:37:41 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-pkg install bash -y
-pkg install libwebp -y
-pkg install git -y
-pkg install nodejs -y
-pkg install ffmpeg -y
-pkg install wget -y 
-pkg install imagemagick -y
+pkg install bash
+pkg install libwebp
+pkg install git
+pkg install nodejs
+pkg install ffmpeg 
+pkg install wget 
+pkg install imagemagick
